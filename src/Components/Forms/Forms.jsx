a8bed6efa7e56df1518456149257863b1ace81ac
Pete Du Beau <peterdubeau@yahoo.com>
2021-03-09 11:31:39 -0500

    basic forms set up

--- src/Components/Forms/Forms.jsx
+++ src/Components/Forms/Forms.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Forms() {
+  const [formData, setFormData] = useState({
+    clientFirstName: '',
+    clientLastName: '',
+    email: '',
+    phone: '',
+    additionalNotes: '',
+    pest: ''
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.currentTarget;
+    console.log(value)
+    setFormData({ ...formData, [name]: value })
+    e.preventDefault()
+  }
+
+  const handleSubmit = () => {
+
+  }
+
   return (
-    <div>
-      Here's a form ok
-    </div>
+    <form>
+      <label>
+        <p>First Name:</p><input
+          type="text"
+          name="clientFirstName"
+          value={formData.clientFirstName}
+          onChange={handleChange}
+        />
+        <p>Last Name:</p><input
+          type="text"
+          name="clientLastName"
+          value={formData.clientLastName}
+          onChange={handleChange}
+        />
+        <br />
+      <button onClick>Submit</button>
+      </label>
+    </form>
   )
 }
