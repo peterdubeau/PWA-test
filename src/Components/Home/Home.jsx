5ea8a6be89b5ac71b46b274df7fc01729b1c2dd0
Pete Du Beau <peterdubeau@yahoo.com>
2021-03-09 14:02:00 -0500

    Added Pestech icons

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import logo from '../../Assets/logo.png'
+import './Home.css'
 
 export default function Home() {
   return (
-    <div>
-      <Link to='/forms'>Forms</Link>
-      <Link to='/another-page'>Another Page</Link>
+    <div className="home-container">
+      <img src={logo} alt='logo' className="logo"/>
+      <h2><Link to='/forms'>Forms</Link></h2>
+      <h2><Link to='/another-page'>Another Page</Link></h2>
     </div>
   )
 }
