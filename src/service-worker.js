5ed9c77aed42539bd174e13304f47b176debe5cb
Pete Du Beau <peterdubeau@yahoo.com>
2021-03-11 09:12:51 -0500

    cache test

--- src/service-worker.js
+++ src/service-worker.js
@@ -61,6 +61,22 @@ registerRoute(
   })
 );
 
+window.addEventListener('activate', function(event) {
+  event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+          return Promise.all(
+              cacheNames.filter(function(cacheName) {
+                  // Return true if you want to remove this cache,
+                  // but remember that caches are shared across
+                  // the whole origin
+              }).map(function(cacheName) {
+                  return caches.delete(cacheName);
+              })
+          );
+      })
+  );
+});
+
 // This allows the web app to trigger skipWaiting via
 // registration.waiting.postMessage({type: 'SKIP_WAITING'})
 self.addEventListener('message', (event) => {
