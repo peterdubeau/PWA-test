6d50217e27ffcb34839c9e9c122788f46bbfda2e
Pete Du Beau <peterdubeau@yahoo.com>
2021-03-11 09:23:39 -0500

    buxfix, return true on update

--- src/service-worker.js
+++ src/service-worker.js
@@ -65,7 +65,8 @@ window.addEventListener('activate', function(event) {
   event.waitUntil(
       caches.keys().then(function(cacheNames) {
           return Promise.all(
-              cacheNames.filter(function(cacheName) {
+            cacheNames.filter(function (cacheName) {
+                return true
                   // Return true if you want to remove this cache,
                   // but remember that caches are shared across
                   // the whole origin
